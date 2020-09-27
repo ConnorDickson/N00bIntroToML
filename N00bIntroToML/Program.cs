647a8c0027b3b7414df7d39093e6115af5050b4d
Connor Dickson <cdickson14@qub.ac.uk>
2020-09-27 11:01:45 +0100

    Initial commit for Blog

---
+++ N00bIntroToML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace N00bIntroToML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PreProcessing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PreProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvBuilder = new StringBuilder();
+
+            foreach (var directory in Directory.EnumerateDirectories(@"..\..\..\RawData"))
+            {
+                //The folders are broken down into their categories
+                var categoryName = Path.GetFileName(directory);
+
+                foreach(var file in Directory.EnumerateFiles(directory))
+                {
+                    var fileContent = File.ReadAllText(file);
+
+                    //Remove the new lines from the content
+                    fileContent = fileContent.Replace("\n", string.Empty).Replace(",", string.Empty);
+
+                    csvBuilder.AppendLine($"{fileContent},{categoryName}");
+                }
+            }
+
+            var csvFilePath = @"..\..\..\RawData\CleanBBCData.csv";
+            File.Delete(csvFilePath);
+            File.AppendAllText(csvFilePath, csvBuilder.ToString());
+        }
+    }
+}
